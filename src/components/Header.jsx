550f6e62ffae01ee20fe01791458b554221903ec
dmnmkua <dmn.mk.ua@gmail.com>
2018-07-21 22:22:24 +0300

    Add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Header extends Component {
+	render() {
+		return (
+			<header class="header">
+				On this place will be new sushi site!!!
+			</header>
+		)
+	}
+}
