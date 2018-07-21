c90303b07c34ea63025cc0f76c2d9f084910bb5d
dmnmkua <dmn.mk.ua@gmail.com>
2018-07-22 00:48:10 +0300

    experiment with data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class Header extends Component {
 	render() {
 		return (
-			<header class="header">
+			<header className="header">
 				On this place will be new sushi site!!!
 			</header>
 		)
--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
+const hrefData = 'https://spreadsheets.google.com/feeds/list/1YDRS3wNHewTTo_MmL9wd9H2WIidLlhGeP_Bx_V9C3QI/od6/public/values?alt=json';
+
+const data = fetch(hrefData)
+	.then((response) => response.json());
+console.log(data);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
